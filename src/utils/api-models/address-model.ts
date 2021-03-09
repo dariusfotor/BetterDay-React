74d22a80a909a9baae87cec381bcf9be837e4b73
Giedrius Grabauskas <giedrius.grabauskas@voicehit.com>
2021-03-09 15:06:16 +0200

    Added address.

---
+++ src/utils/api-models/address-model.ts
@@ -0,0 +1,7 @@
+export interface AddressModel {
+    address1: string;
+    address2?: string;
+    zipCode: string;
+    city: string;
+    state: string;
+}
--- src/utils/api-models/patient-details-model.ts
+++ src/utils/api-models/patient-details-model.ts
@@ -1,7 +1,11 @@
+import { AddressModel } from "./address-model";
+
 export interface PatientDetailsModel {
     id: number;
     firstName: string;
     lastName: string;
     image: string;
     dateOfBirth: string;
+    phoneNumber: string;
+    address?: AddressModel;
 }
